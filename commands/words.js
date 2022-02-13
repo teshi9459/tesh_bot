06e1dc9c346b8dc9702277fd5453c127ff832018
Teshi <74679134+djario@users.noreply.github.com>
2022-02-13 16:03:08 +0100

    refresh words sc

--- commands/words.js
+++ commands/words.js
@@ -1,110 +1,93 @@
 const {
- SlashCommandBuilder
+   SlashCommandBuilder
 } = require('@discordjs/builders');
 const db = require('../libs/db');
 const dc = require('../libs/dc');
 const shorts = require('../libs/shorts');
 module.exports = {
- data: new SlashCommandBuilder()
- .setName('words')
- .setDescription('Einstellungen für words')
- .addSubcommand(subcommand =>
-  subcommand
-  .setName('max')
-  .setDescription('Anzahl an Wörten die mindestens erreicht werden müssen')
-  .addIntegerOption(option => option.setName('amount').setDescription('Zahl min 1').setRequired(true)))
- .addSubcommand(subcommand =>
-  subcommand
-  .setName('min')
-  .setDescription('Anzahl an Wörtern ab denen das Modul startet').addIntegerOption(option => option.setName('amount').setDescription('Zahl min 0').setRequired(true)))
- .addSubcommand(subcommand =>
-  subcommand
-  .setName('text')
-  .setDescription('setzt den Text welcher in der Warnung steht').addStringOption(option => option.setName('text').setDescription('text DC Syntax wie Sternchen können benutzt werden').setRequired(true)))
- .addSubcommand(subcommand =>
-  subcommand
-  .setName('deletetime')
-  .setDescription('Zeit nach der die Warnung gelöscht wird').addIntegerOption(option => option.setName('time').setDescription('Zeit in Sekunden').setRequired(true)))
- .addSubcommand(subcommand =>
-  subcommand
-  .setName('reportlevel')
-  .setDescription('setzt die Schwere des Verstoßes').addIntegerOption(option => option.setName('level').setDescription('höhe des Levels').setRequired(true)))
- .addSubcommand(subcommand =>
-  subcommand
-  .setName('status')
-  .setDescription('gibt die Moduldaten zurück'))
- .addSubcommand(subcommand =>
-  subcommand
-  .setName('setup')
-  .setDescription('erstellt das words Modul'))
- .addSubcommand(subcommand =>
-  subcommand
-  .setName('on')
-  .setDescription('schaltet das Modul ein'))
- .addSubcommand(subcommand =>
-  subcommand
-  .setName('off')
-  .setDescription('schaltet das Modul aus')),
- async execute(client, interaction) {
-  let server;
-  let module;
-  try {
-   server = db.getServer(interaction.guildId);
-  } catch (e) {
-   console.error(e);
-   interaction.reply('bitte starte zuerst `/setup`');
-   return;
-  }
-  
-  if (!interaction.member.roles.cache.has(server.adminrole)) return;
-  try {
-   module = db.getModuleS(server, 'words');
-  } catch (e) {
-   if (interaction.options.getSubcommand() != 'setup') {
-    interaction.reply('bitte starte zuerst `/words setup`');
-    return;
-   }
-  }
+   data: new SlashCommandBuilder()
+      .setName('words')
+      .setDescription('Einstellungen für words')
+      .addSubcommand(subcommand =>
+         subcommand
+            .setName('max')
+            .setDescription('Anzahl an Wörten die mindestens erreicht werden müssen')
+            .addIntegerOption(option => option.setName('amount').setDescription('Zahl min 1').setRequired(true)))
+      .addSubcommand(subcommand =>
+         subcommand
+            .setName('min')
+            .setDescription('Anzahl an Wörtern ab denen das Modul startet').addIntegerOption(option => option.setName('amount').setDescription('Zahl min 0').setRequired(true)))
+      .addSubcommand(subcommand =>
+         subcommand
+            .setName('status')
+            .setDescription('gibt die Moduldaten zurück'))
+      .addSubcommand(subcommand =>
+         subcommand
+            .setName('setup')
+            .setDescription('erstellt das words Modul'))
+      .addSubcommand(subcommand =>
+         subcommand
+            .setName('power')
+            .setDescription('schaltet das Modul ein oder aus')
+            .addBooleanOption(option => option.setName('status').setDescription('an = true aus = false').setRequired(true))),
+   async execute(client, interaction) {
+      let server;
+      let module;
+      try {
+         server = db.getServer(interaction.guildId);
+      } catch (e) {
+         console.error(e);
+         interaction.reply('bitte starte zuerst `/setup`');
+         return;
+      }
 
-  let answer = `>>> ${interaction.options.getSubcommand()} ist jetzt \``;
-  switch (interaction.options.getSubcommand()) {
-   case 'max':
-    module.max = interaction.options.getInteger('amount');
-    answer += module.max+ '`';
-    break;
-   case 'min':
-    module.min = interaction.options.getInteger('amount');
-    answer += module.min+ '`';
-    break;
-   case 'text':
-    module.txt = interaction.options.getString('text');
-    answer += module.txt+ '`';
-    break;
-   case 'deletetime':
-    module.delTime = interaction.options.getInteger('time')*1000;
-    answer += module.delTime+ '`';
-    break;
-   case 'reportlevel':
-    module.max = interaction.options.getInteger('level');
-    answer += module.reportLevel+ '`';
-    break;
-   case 'status':
-    answer = "__Module__:\n\`\`\`"+JSON.stringify(module)+ '```';
-    break;
-   case 'setup':
-    shorts.wordsSetup(interaction);
-    return;
+      if (!interaction.member.roles.cache.has(server.adminrole)) return;
+      try {
+         module = db.getModuleS(server, 'words');
+      } catch (e) {
+         if (interaction.options.getSubcommand() != 'setup') {
+            interaction.reply('bitte starte zuerst `/words setup`');
+            return;
+         }
+      }
 
-    case 'on':
-     module.enabled = true;
-     answer = 'Modul words ist nun `AN`';
-     break;
-    case 'off':
-     module.enabled = false;
-     answer = 'Modul words ist nun `AUS`';
-     break;
-  }
-  db.updateModuleS(server, module);
-  interaction.reply(answer);
- },
+      let answer = `>>> ${interaction.options.getSubcommand()} ist jetzt \``;
+      switch (interaction.options.getSubcommand()) {
+         case 'max':
+            module.max = interaction.options.getInteger('amount');
+            answer += module.max + '`';
+            break;
+         case 'min':
+            module.min = interaction.options.getInteger('amount');
+            answer += module.min + '`';
+            break;
+         case 'text':
+            module.txt = interaction.options.getString('text');
+            answer += module.txt + '`';
+            break;
+         case 'deletetime':
+            module.delTime = interaction.options.getInteger('time') * 1000;
+            answer += module.delTime + '`';
+            break;
+         case 'reportlevel':
+            module.max = interaction.options.getInteger('level');
+            answer += module.reportLevel + '`';
+            break;
+         case 'status':
+            answer = "__Module__:\n\`\`\`" + JSON.stringify(module) + '```';
+            break;
+         case 'setup':
+            shorts.wordsSetup(interaction);
+            return;
+
+         case 'power':
+            module.enabled = interaction.getBoolean('status');
+            answer = 'Modul words ist nun `Aus`';
+            if (interaction.getBoolean('status'))
+               answer = 'Modul words ist nun `An`';
+            break;
+      }
+      db.updateModuleS(server, module);
+      interaction.reply(answer);
+   },
 };
