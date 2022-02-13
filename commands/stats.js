0c4505356cd3edc82ed1f2ff96db4bf377b87836
Teshi <74679134+djario@users.noreply.github.com>
2022-02-13 17:18:13 +0100

    fix

--- commands/stats.js
+++ commands/stats.js
@@ -1,25 +1,25 @@
 const {
- SlashCommandBuilder
+    SlashCommandBuilder
 } = require('@discordjs/builders');
 const db = require('../libs/db');
-const dc = require('../libs/dc');
 const stats = require('../message/stats');
 module.exports = {
- data: new SlashCommandBuilder()
- .setName('stats')
- .setDescription('only Teshi - startet Analytics'),
- async execute(client,
-  interaction) {
-  let server;
-  try {
-   server = db.getServer(interaction.guildId);
-  } catch (e) {
-   console.error(e);
-   interaction.reply('bitte starte zuerst `/setup`');
-   return;
-  }
+    data: new SlashCommandBuilder()
+        .setName('stats')
+        .setDescription('only Teshi - startet Analytics')
+        .setDefaultPermission(false),
+    async execute(client,
+        interaction) {
+        let server;
+        try {
+            server = db.getServer(interaction.guildId);
+        } catch (e) {
+            console.error(e);
+            interaction.reply('bitte starte zuerst `/setup`');
+            return;
+        }
 
-  if (!interaction.member.roles.cache.has(server.adminrole)) return;
-  stats.setup(interaction);
- },
+        if (!interaction.member.roles.cache.has(server.adminrole)) return;
+        stats.setup(interaction);
+    },
 };
