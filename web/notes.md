f25fd92425221842cda97fbc3110ec48364011ff
djario <dj.mobile.h@gmail.com>
2022-02-20 12:43:49 +0100

    setup ecpress website

---
+++ web/notes.md
@@ -0,0 +1,18 @@
+#fonts
+Google
+handw:
+font-family: 'Comfortaa', cursive;
+code:
+font-family: 'Roboto Mono', monospace;
+broke:
+font-family: 'Source Sans Pro', sans-serif;
+font-family: 'Montserrat', sans-serif;
+
+Css
+code:
+font-family: 'Courier New', monospace;
+general:
+font-family: Geneva, Verdana, sans-serif;
+
+#colors
+tesh: #aaeeff
---
+++ web/web.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const path = require('path');
+const {
+ port
+} = require('../config/config.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var publicPath = path.join(__dirname, 'public');
+
+
+
+
+router.get("/", function (req, res) {
+ res.sendFile(path.join(__dirname, "/"));
+});
+
+
+app.get('/new_deposit', function (req, res) {
+ res.sendfile(publicPath + '/new_deposit.html');
+});
+
+
+app.listen(port, () => console.log(`Web listening at http://localhost:${port}`));
