29299c8c13b369ef80f255fb4599b885a6532b29
djario <dj.mobile.h@gmail.com>
2022-02-22 16:36:21 +0100

    fix ping

--- commands/ping.js
+++ commands/ping.js
@@ -2,13 +2,14 @@ const {
  SlashCommandBuilder
 } = require('@discordjs/builders');
 const dc = require('../libs/dc');
+const ms = require('ms');
 module.exports = {
  data: new SlashCommandBuilder()
  .setName('ping')
  .setDescription('antwortet mit Pong!'),
  async execute(client, interaction) {
-  const rec = interaction.createdTimestamp - new Date();
-  const text = "`"+ rec +"ms` speed\n\n`"+client.ws.ping+"ms` ping\n\n`"+ client.uptime+"ms` uptime"
+  const rec = new Date() - interaction.createdTimestamp;
+  const text = "`"+ rec +"ms` speed\n\n`"+client.ws.ping+"ms` ping\n\n`"+ ms(client.uptime)+"` uptime"
   const embed = dc.makeSimpleEmbed(interaction, "#aaeeff", "Pong!", text);
   await interaction.reply({
    embeds: [embed]});
