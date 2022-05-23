8cae87fdb4c9f532a3dc2ac9ea892ff2b1db64cc
Teshi <dj.mobile.h@gmail.com>
2022-05-23 11:54:23 +0200

    command update

---
+++ commands/settings/info.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("info")
+    .setDescription("Infos und Links"),
+  async execute(client, interaction) {
+    const Embed = new MessageEmbed()
+      .setColor("#aaeeff")
+      .setTitle("Infosheet")
+      .setURL("http://tesh.qg")
+      .setDescription("Hier sind einige Links:")
+      .setThumbnail(
+        "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+      )
+      .addFields(
+        { name: "Github", value: "https://github.com/teshi9459/tesh_bot" },
+        { name: "Teshboard", value: "http://tesh.gq" },
+        { name: "Discord Server", value: "-" }
+      )
+      .setFooter({
+        text:
+          interaction.client.user.username + "-Bot | " + "Made with ♡ by Teshi",
+        iconURL: interaction.guild.iconURL(),
+      });
+    await interaction.reply({ embeds: [Embed] });
+  },
+};
